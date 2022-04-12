c331744e22207e6436c9cf854154bbdfcf9ea2f2
Nandha11498 <103486431+Nandha11498@users.noreply.github.com>
2022-04-12 13:27:35 +0530

    Add files via upload

---
+++ Operator.py
@@ -0,0 +1,20 @@
+print("Addition Function")
+a=int(input("Enter the first no: "))
+b=int(input("Enter the second no: "))
+c= a+b
+print("The Addition value is : ",c)
+print("Subtraction Function")
+a=int(input("Enter the first no: "))
+b=int(input("Enter the second no: "))
+c= a-b
+print("The Subtraction value is : ",c)
+print("Multiplication Function")
+a=int(input("Enter the first no: "))
+b=int(input("Enter the second no: "))
+c= a*b
+print("The Multiplication value is : ",c)
+print("Division Function")
+a=int(input("Enter the first no: "))
+b=int(input("Enter the second no: "))
+c= a/b
+print("The Division value is : ",c)
