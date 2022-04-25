7aa340ab7d37f8bca1c253ba9d9ea69d07ceb58d
Nandha11498 <103486431+Nandha11498@users.noreply.github.com>
2022-04-25 12:04:39 +0530

    Add files via upload

---
+++ Basic  Operators/Assign_Operator.py
@@ -0,0 +1,11 @@
+print("Enter the 1st value: ")
+a=int(input())
+print("Enter the 2nd value: ")
+b=int(input())
+c=0
+c+=a
+print("The addition value is: ",c)
+c-=a
+print("The Subtraction value is: ",c)
+b*=a
+print("The multiplecation values is",c)
---
+++ Basic  Operators/Bitwise_Operator.py
@@ -0,0 +1,12 @@
+a=int(input("Enter the 1st value: "))
+b=int(input("Enter the 2nd value: "))
+c=0
+
+c=a&b;
+print("The AND value is: ",c)
+
+c=a|b
+print("The OR value is : ",c)
+
+c=a^b
+print("The XOR value is :",c)
