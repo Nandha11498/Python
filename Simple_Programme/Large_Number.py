44c69668de23315cb9156ec6c317c5eb4b59f64f
Nandha11498 <103486431+Nandha11498@users.noreply.github.com>
2022-04-25 12:37:47 +0530

    Add files via upload

---
+++ Simple_Programme/Large_Number.py
@@ -0,0 +1,14 @@
+print("\n Largest in 3 Numbers ")
+
+print ("\n Enter any 3 Numbers ")
+a=int(input())
+b=int(input())
+c=int(input())
+
+if a>b and a>c:
+    print("\n The Largest Number is A : ",a)
+elif b>a and b>c:
+    print ("\n The Largest Numbers is B : ",b)
+else:
+    print ("\n The Largest Number is C : ",c)
+
