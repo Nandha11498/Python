5f05a22e52e5b2e9a26f463eaacdd1bc69d4c2fb
Nandha11498 <103486431+Nandha11498@users.noreply.github.com>
2022-05-09 11:56:07 +0530

    Add files via upload

---
+++ Function/Support.py
@@ -0,0 +1,5 @@
+def print_func( a,b ):
+   c=a+b
+   
+   print ("Sum : ", c)
+   return
---
+++ Function/sub.py
@@ -0,0 +1,3 @@
+import Support
+# Now you can call defined function that module as follows
+Support.print_func(5,6)
